098ee7d83a5022abaed1122a3d5474384fbfef29
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-08-07 20:03:39 +0300

    initial tests

--- HeadphonesTest/Form1.cs
+++ HeadphonesTest/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,5 +18,12 @@ namespace HeadphonesTest
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SystemSounds.Question.Play();
+
+            SoundPlayer simpleSound = new SoundPlayer(@"pack://application:,,,/Sounds\6.wav");
+            simpleSound.Play();
+        }
     }
 }
